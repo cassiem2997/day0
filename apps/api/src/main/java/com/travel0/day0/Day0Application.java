e7b6216d1a1de0ee5344a66e6cfed486dde8ca46
yumi <yd10407@naver.com>
2025-08-20 10:39:20 +0900

    github initial commit

---
+++ apps/api/src/main/java/com/travel0/day0/Day0Application.java
@@ -0,0 +1,13 @@
+package com.travel0.day0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day0Application.class, args);
+	}
+
+}
---
+++ apps/api/src/test/java/com/travel0/day0/Day0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel0.day0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
