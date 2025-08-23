e74f0789f557c096e13bf5433922b1a228b6d8d1
LeeJongHyun <a24358232@gmail.com>
2025-08-23 20:29:36 +0900

    fix : 랜딩페이지1 수정

--- apps/web/src/pages/Landing/LandingHero.tsx
+++ apps/web/src/pages/Landing/LandingHero.tsx
@@ -1,72 +1,26 @@
+// pages/Landing/LandingHero.tsx
 import styles from "./LandingHero.module.css";
 
-// svg import
-import clouds from "../../assets/clouds.svg";
-import header from "../../assets/ChecklistHeader.svg";
-import school from "../../assets/school.svg";
-import leftTree from "../../assets/leftTree.svg";
-import rightTree from "../../assets/rightTree.svg";
-import road from "../../assets/road.svg";
+import bg from "../../assets/landing1.svg";      // 새로 뽑은 배경 1장
+import clouds from "../../assets/clouds.svg";    // 구름
 import walkingLeft from "../../assets/walkingLeft.svg";
 import walkingRight from "../../assets/walkingRight.svg";
 
 export default function LandingHero() {
   return (
     <section className={styles.stage}>
-      {/* 하늘 배경 */}
-      <div className={styles.sky}></div>
+      <img className={styles.bg} src={bg} alt="background" />
 
-      {/* 구름 */}
-      <img
-        className={`${styles.cloud} ${styles.cloudA}`}
-        src={clouds}
-        alt="clouds"
-      />
-      <img
-        className={`${styles.cloud} ${styles.cloudB}`}
-        src={clouds}
-        alt="clouds"
-      />
+      {/* 구름 2장 (부유 애니메이션) */}
+      <img className={`${styles.cloud} ${styles.cloudA}`} src={clouds} alt="" />
+      <img className={`${styles.cloud} ${styles.cloudB}`} src={clouds} alt="" />
 
-      {/* 타이틀 */}
-      <img className={styles.header} src={header} alt="CHECKLISTS" />
+      {/* 캐릭터: 제자리 앞걸음 */}
+      <img className={`${styles.walker} ${styles.walkLeft}`} src={walkingLeft} alt="" />
+      <img className={`${styles.walker} ${styles.walkRight}`} src={walkingRight} alt="" />
 
-      {/* 학교 / 나무 */}
-      <img className={styles.school} src={school} alt="school" />
-      <img
-        className={`${styles.tree} ${styles.leftTree}`}
-        src={leftTree}
-        alt="left tree"
-      />
-      <img
-        className={`${styles.tree} ${styles.rightTree}`}
-        src={rightTree}
-        alt="right tree"
-      />
-
-      {/* 도로 */}
-      <img className={styles.road} src={road} alt="road" />
-
-      {/* 캐릭터 */}
-      <img
-        className={`${styles.walker} ${styles.walkLeft}`}
-        src={walkingLeft}
-        alt="walker left"
-      />
-      <img
-        className={`${styles.walker} ${styles.walkRight}`}
-        src={walkingRight}
-        alt="walker right"
-      />
-
-      {/* 바닥 */}
-      <div className={styles.ground}></div>
-
-      {/* CTA */}
       <div className={styles.ctaWrap}>
-        <a href="/signup" className={styles.cta}>
-          시작하기
-        </a>
+        <a className={styles.cta} href="/signup">시작하기</a>
       </div>
     </section>
   );
