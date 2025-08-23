200d61d8deccb39149a36315af0e088d6ea71f93
LeeJongHyun <a24358232@gmail.com>
2025-08-23 22:00:07 +0900

    fix : 캐릭터 애니메이션 수정

--- apps/web/src/pages/Landing/LandingHero.tsx
+++ apps/web/src/pages/Landing/LandingHero.tsx
@@ -1,26 +1,42 @@
 // pages/Landing/LandingHero.tsx
 import styles from "./LandingHero.module.css";
 
-import bg from "../../assets/landing1.svg";      // 새로 뽑은 배경 1장
-import clouds from "../../assets/clouds.svg";    // 구름
+import bg from "../../assets/landing1.svg"; // 배경 1장
+import clouds from "../../assets/clouds.svg"; // 구름
 import walkingLeft from "../../assets/walkingLeft.svg";
 import walkingRight from "../../assets/walkingRight.svg";
 
 export default function LandingHero() {
   return (
     <section className={styles.stage}>
+      {/* 배경 */}
       <img className={styles.bg} src={bg} alt="background" />
 
-      {/* 구름 2장 (부유 애니메이션) */}
+      {/* 구름 */}
       <img className={`${styles.cloud} ${styles.cloudA}`} src={clouds} alt="" />
       <img className={`${styles.cloud} ${styles.cloudB}`} src={clouds} alt="" />
 
-      {/* 캐릭터: 제자리 앞걸음 */}
-      <img className={`${styles.walker} ${styles.walkLeft}`} src={walkingLeft} alt="" />
-      <img className={`${styles.walker} ${styles.walkRight}`} src={walkingRight} alt="" />
+      {/* 캐릭터: 역동적인 미니 페이스 */}
+      <div className={styles.walkerStage}>
+        <div className={styles.walkerRig}>
+          <img
+            className={`${styles.walker} ${styles.walkLeft}`}
+            src={walkingLeft}
+            alt=""
+          />
+          <img
+            className={`${styles.walker} ${styles.walkRight}`}
+            src={walkingRight}
+            alt=""
+          />
+        </div>
+      </div>
 
+      {/* CTA */}
       <div className={styles.ctaWrap}>
-        <a className={styles.cta} href="/signup">시작하기</a>
+        <a className={styles.cta} href="/signup">
+          시작하기
+        </a>
       </div>
     </section>
   );
