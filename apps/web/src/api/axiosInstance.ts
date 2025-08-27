4200d9e4f5e321066fc97f72ac792951ff36678b
LeeJongHyun <a24358232@gmail.com>
2025-08-27 23:31:57 +0900

    fix : 프론트쪽 타임아웃 시간 연장

--- apps/web/src/api/axiosInstance.ts
+++ apps/web/src/api/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError } from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080",
-  timeout: 8000,
+  timeout: 30000,
   withCredentials: true, // 쿠키로 변경
   headers: { Accept: "application/json" },
 });
