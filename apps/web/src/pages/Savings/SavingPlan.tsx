57a2c38e658d2d5f633f0ac9214b14af9da72be0
LeeJongHyun <a24358232@gmail.com>
2025-08-29 17:13:42 +0900

    feat : 적금플랜페이지 추가

---
+++ apps/web/src/pages/Savings/SavingPlan.tsx
@@ -0,0 +1,105 @@
+import "./style.css";
+
+export const SavingPlan = () => {
+  return (
+    <div className="screen">
+      <div className="div">
+        <img className="line" alt="Line" src="/img/line-1-3.svg" />
+
+        <img className="img" alt="Line" src="/img/line-1-3.svg" />
+
+        <img className="line-2" alt="Line" src="/img/line-1-3.svg" />
+
+        <img
+          className="iconly-light-outline"
+          alt="Iconly light outline"
+          src="/img/iconly-light-outline-profile-1.svg"
+        />
+
+        <div className="frame">
+          <img className="union" alt="Union" src="/img/union-12.svg" />
+
+          <div className="overlap">
+            <img className="union-2" alt="Union" src="/img/union-13.svg" />
+
+            <div className="text-wrapper">적금 플랜</div>
+          </div>
+
+          <div className="overlap-group">
+            <img className="union-3" alt="Union" src="/img/union-14.svg" />
+
+            <div className="frame-2">
+              <div className="div-wrapper">
+                <div className="text-wrapper-2">계좌조회</div>
+              </div>
+
+              <div className="overlap-2">
+                <div className="text-wrapper-3">이자 그게 뭔데 수시입출금</div>
+
+                <div className="text-wrapper-4">123-4567-890</div>
+              </div>
+
+              <div className="overlap-group-2">
+                <div className="text-wrapper-5">납입 주기</div>
+              </div>
+
+              <div className="overlap-3">
+                <div className="text-wrapper-6">납일 일</div>
+              </div>
+
+              <div className="overlap-4">
+                <div className="overlap-5">
+                  <div className="text-wrapper-7">주별</div>
+                </div>
+
+                <div className="overlap-6">
+                  <div className="text-wrapper-8">월별</div>
+                </div>
+              </div>
+
+              <div className="overlap-7">
+                <div className="text-wrapper-9">15일</div>
+              </div>
+
+              <div className="overlap-8">
+                <div className="text-wrapper-10">기간</div>
+              </div>
+
+              <div className="overlap-9">
+                <div className="text-wrapper-11">생성하기</div>
+              </div>
+
+              <div className="overlap-10">
+                <div className="text-wrapper-12">개월</div>
+
+                <div className="text-wrapper-13">5</div>
+              </div>
+
+              <div className="overlap-11">
+                <div className="text-wrapper-14">100,000</div>
+
+                <div className="text-wrapper-15">원</div>
+              </div>
+
+              <div className="overlap-12">
+                <p className="p">
+                  <span className="span">납일 액 </span>
+
+                  <span className="text-wrapper-16">(원)</span>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <img className="union-4" alt="Union" src="/img/union-15.svg" />
+        </div>
+
+        <div className="overlap-13">
+          <div className="text-wrapper-17">출국준비의 기본! 적금 플랜!</div>
+        </div>
+
+        <div className="text-wrapper-18">D - 적금</div>
+      </div>
+    </div>
+  );
+};
